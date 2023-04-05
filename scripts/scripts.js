c6a31b84b18a092b91d6a9457d52fd3fca90a44e
TPoteat775 <49031681+TPoteat775@users.noreply.github.com>
2023-04-04 22:39:27 -0400

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,6 @@
+//FIND DAYS BETWEEN MINDATE AND MAX DATE
+//DISPLAY ALL EVENTS FOR LEAVE and PERFORM CALCULATION
+
 let currentMonth = 0;
 let clicked = null;
 let records = localStorage.getItem('records') ? JSON.parse(localStorage.getItem('records')) : [];
@@ -7,19 +10,22 @@ const newLeaveEntry = document.getElementById('create-leave-entry');
 const deleteLeaveEntry = document.getElementById('delete-leave-entry')
 const leaveText = document.getElementById('leave-text');
 const backdrop = document.getElementById('backdrop');
-const hoursPerEntry = document.getElementById('hours-total');
+//const hoursPerEntry = document.getElementById('hours-total');
 
 const annualLeave = document.getElementById('annual-leave');
 const sickLeave = document.getElementById('sick-leave');
 const creditHrs = document.getElementById('credit-hrs');
 const compHrs = document.getElementById('comp-hrs');
 
-const minDate = document.getElementById('min-date');
-const maxDate = document.getElementById('max-date');
+let minDate = document.getElementById('min-date');
+let maxDate = document.getElementById('max-date');
 
 const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 function createLeaveEntry(date){
+  let splitDate = formatDate(date);
+  minDate.setAttribute('value', splitDate);
+  maxDate.setAttribute('value', splitDate);
   clicked = date;
 
   const leaveEntry = records.find(e => e.date === clicked);
@@ -31,6 +37,26 @@ function createLeaveEntry(date){
     newLeaveEntry.style.display = 'block';
   }
   backdrop.style.display = 'block';
+}
+
+function formatDate(date){
+  let splitDate = date.split('/');
+  let tempDate = "";
+
+  //splitDate[0] = month 
+  //splitDate[1] = day
+  //splitDate[2] = year
+
+  if(splitDate[0] < 10 && splitDate[1] < 10){
+    tempDate = splitDate[2] + '-0' + splitDate[0] + '-0' + splitDate[1];
+  } else if(splitDate[0] < 10 && splitDate[1] >= 10) {
+    tempDate = splitDate[2] + '-0' + splitDate[0] + '-' + splitDate[1];
+  } else if(splitDate[0] >= 10 && splitDate[1] < 10) {
+    tempDate = splitDate[2] + '-' + splitDate[0] + '-0' + splitDate[1];
+  }else{
+    tempDate = splitDate[2] + '-' + splitDate[0] + '-' + splitDate[1];
+  } 
+  return tempDate
 }
 
 function load() {
@@ -49,7 +75,6 @@ function load() {
   const daysInMonth = new Date(year, month + 1, 0).getDate();
 
   const lastDayInMonth = new Date(year, month, daysInMonth);
-
   
   const dateString = firstDayOfMonth.toLocaleDateString('en-us', {
     weekday: 'long',
@@ -65,35 +90,17 @@ function load() {
     day: 'numeric',
   });
 
-  let tempDate = "";
-
-  if(month+1 < 10 && day < 10){
-    tempDate = year + '-0' + month + '-0' + day;
-  } else if(month+1 < 10 && day >= 10) {
-    tempDate = year + '-0' + month + '-' + day;
-  } else if(month+1 >= 10 && day < 10) {
-    tempDate = year + '-' + month + '-0' + day;
-  }else{
-    tempDate = year + '-' + month + '-' + day;
-  }
-
-  minDate.setAttribute('min', tempDate);
-  maxDate.setAttribute('min', tempDate);
-
-
   const endPaddingDays = weekdays.length - (weekdays.indexOf(endDateString.split(', ')[0]) + 1);
 
   const paddingDays = weekdays.indexOf(dateString.split(', ')[0]);
 
-  //console.log("Padding Days: " + paddingDays);
-
-  //console.log(endPaddingDays);
-
+  //Displays current month in header
   document.getElementById('monthDisplay').innerText = 
     `${dt.toLocaleDateString('en-us', {month: 'long'})} ${year}`;
 
   calendar.innerHTML = '';
 
+  //Set days of the weeks
   for(let i=0; i<7; i++) {
     const weekDaySquare = document.createElement('div');
     weekDaySquare.classList.add('daysOfTheWeek');
@@ -102,30 +109,35 @@ function load() {
     calendar.appendChild(weekDaySquare);
   }
 
+  //Set days and paddingDays for current month
   for(let i = 1; i <= paddingDays + daysInMonth + endPaddingDays; i++) {
     const daySquare = document.createElement('div');
     daySquare.classList.add('day');
 
     const dayString = `${month + 1}/${i - paddingDays}/${year}`;
 
-    //console.log(dayString)
-
+    //CHANGE THIS CODE TO EXTEND EVENT!!!
+    //If not a padding day, add number
     if (i > paddingDays && i - paddingDays <= daysInMonth) {
       daySquare.innerText = i - paddingDays;
       const leaveEntry = records.find(e => e.date === dayString);
 
+      //Set the current day of the month
       if (i - paddingDays === day && currentMonth === 0) {
         daySquare.id = 'currentDay';
+        //Current date is the date leave is allowed
+        let minDateForLeave = formatDate(dayString);
+        minDate.setAttribute('min', (minDateForLeave));
+        maxDate.setAttribute('min', (minDateForLeave));
       }
 
-      //CHANGE THIS CODE TO EXTEND EVENT!!!
+
       if (leaveEntry) {
         const leaveDiv = document.createElement('div');
         leaveDiv.classList.add('event');
         leaveDiv.innerText = leaveEntry.title;
         daySquare.appendChild(leaveDiv);
       }
-
       daySquare.addEventListener('click', () => createLeaveEntry(dayString));
     } else {
       daySquare.classList.add('padding');
@@ -145,16 +157,33 @@ function cancelEntry(){
 }
 
 function submitLeave(){
-  const totalHrs = document.getElementById('total-hrs')
+  const hrsPerDay = document.getElementById('hrs-per-day')
+  const leaveType = document.getElementById('leave-type');
+  let days = 1;
+  let flag = false;
 
-  console.log(totalHrs.value);
+  minDate = document.getElementById('min-date');
+  maxDate = document.getElementById('max-date');
+  
 
-  if (totalHrs.value) {
-    totalHrs.classList.remove('error');
+  if(minDate.value > maxDate.value ) {
+    alert("Minimum date can not be larger than maximum date")
+    minDate.classList.add('error');
+    maxDate.classList.add('error');
+    flag = false;
+  } else{
+    minDate.classList.remove('error');
+    maxDate.classList.remove('error');
+    flag = true;
+    //days = daysForRecord(minDate, maxDate);
+  }
+
+  if (flag && hrsPerDay.value != 0) {
+    hrsPerDay.classList.remove('error');
 
     records.push({
       date: clicked,
-      title: leaveText.value,
+      title: leaveType.value + ' - ',
     });
 
     //"date + " "
@@ -162,11 +191,41 @@ function submitLeave(){
     localStorage.setItem('records', JSON.stringify(records));
     cancelEntry();
   } else {
-    totalHrs.classList.add('error');
+    hrsPerDay.classList.add('error');
   }
 }
 
-function validateLeave(){
+function validateEntry(hours, leaveType){
+  let tempNum = 0;
+
+  if(hours > 8 || hours < 0) {
+    alert("The hours value must be between 0-8");
+  }
+
+  if(leaveType=="sick"){
+    tempNum = 
+    alert('Error. Not enough hours in Sick Leave.')
+
+  } else if(leaveType=="annual"){
+
+  } else if(leaveType=="credit"){
+
+  }else if(leaveType=="comp"){
+
+  }else{
+    alert('Invalid entry. Please try again.');
+  }
+
+}
+
+function daysForRecord(minDate, maxDate){
+
+  //splitDate1[0] is year
+  //splitDate1[1] is month
+  //splitDate1[2] is day
+  
+  let splitDate1 = minDate.value.split('-');
+  let splitDate2 = maxDate.value.split('-');
 
 }
 
